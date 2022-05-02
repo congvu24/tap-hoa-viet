b91b9bdb65e0a087d300bcb6b8ac9f64079447a8
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2022-05-02 17:06:13 +0700

    done products screen UI

--- App.js
+++ App.js
@@ -3,12 +3,14 @@ import {Provider} from 'react-redux';
 import {store, persistor} from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
 import AppRoute from './src/route';
+import ProductsScreen from './src/screens/ProductsScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <AppRoute />
+        {/* <AppRoute /> */}
+        <ProductsScreen />
       </PersistGate>
     </Provider>
   );
