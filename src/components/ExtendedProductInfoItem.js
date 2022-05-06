ba22c11f5806cc4c0acb1efd6cde0333d431be50
Thanh Hien Tran <71955948+HinTran013@users.noreply.github.com>
2022-05-06 17:07:47 +0700

    add product detail screen

---
+++ src/components/ExtendedProductInfoItem.js
@@ -0,0 +1,49 @@
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import React, {useState} from 'react';
+import {Icon} from 'react-native-elements';
+import {TEXT_COLOR} from '../constants/Colors';
+
+const ExtendedProductInfoItem = ({title, description}) => {
+  const [isShowDescription, setIsShowDescription] = useState(false);
+
+  const handleShowDescription = () => setIsShowDescription(!isShowDescription);
+
+  return (
+    <TouchableOpacity style={styles.container} onPress={handleShowDescription}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.title}>{title}</Text>
+        <Icon
+          type="material"
+          name={
+            !isShowDescription ? 'keyboard-arrow-down' : 'keyboard-arrow-up'
+          }
+        />
+      </View>
+
+      {isShowDescription && (
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.description}>{description}</Text>
+        </View>
+      )}
+    </TouchableOpacity>
+  );
+};
+
+export default ExtendedProductInfoItem;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  title: {fontSize: 16, color: TEXT_COLOR, fontWeight: '500'},
+  descriptionContainer: {marginTop: 10},
+  description: {fontSize: 18, flex: 0.6},
+});
--- src/screens/ProductDetailsScreen.js
+++ src/screens/ProductDetailsScreen.js
@@ -1,7 +1,16 @@
 import {StyleSheet, Text, View, ScrollView} from 'react-native';
 import React from 'react';
 import ProductDetailsHeader from '../components/ProductDetailsHeader';
-import ProductDetailsImages from '../components/ProductDetailsImages';
+import {TEXT_COLOR, PRIMARY_COLOR} from '../constants/Colors';
+import {SliderBox} from 'react-native-image-slider-box';
+import ExtendedProductInfoItem from '../components/ExtendedProductInfoItem';
+
+const images = [
+  'https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+  'https://images.unsplash.com/photo-1607522370275-f14206abe5d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1121&q=80',
+  'https://images.unsplash.com/photo-1581017316471-1f6ef7ce6fd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
+  'https://images.unsplash.com/photo-1552066344-2464c1135c32?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+];
 
 const ProductDetailsScreen = ({
   productName = 'Giày thể thao cá tính',
@@ -14,11 +23,15 @@ const ProductDetailsScreen = ({
   numberOfProduct = 0,
 }) => {
   return (
-    <ScrollView>
+    <View style={styles.container}>
       <ProductDetailsHeader productCode={productCode} />
-      <ScrollView>
-        <View style={styles.carouselContainer}>
-          <ProductDetailsImages />
+      <ScrollView style={styles.scrolledContainer}>
+        <View style={styles.imageSliderContainer}>
+          <SliderBox
+            images={images}
+            sliderBoxHeight={250}
+            dotColor={PRIMARY_COLOR}
+          />
         </View>
 
         <View style={styles.productInfoContainer}>
@@ -28,55 +41,118 @@ const ProductDetailsScreen = ({
           <View style={styles.detailsContainer}>
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Mã hàng</Text>
-              <Text>{productCode}</Text>
+              <Text style={styles.simpleText}>{productCode}</Text>
             </View>
 
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Mã vạch</Text>
-              <Text>{barCode}</Text>
+              <Text style={styles.simpleText}>{barCode}</Text>
             </View>
 
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Nhóm hàng</Text>
-              <Text>{category}</Text>
+              <Text style={styles.simpleText}>{category}</Text>
             </View>
 
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Thương hiệu</Text>
-              <Text>{brand}</Text>
+              <Text style={styles.simpleText}>{brand}</Text>
             </View>
 
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Giá vốn</Text>
-              <Text>{capitalPrice}</Text>
+              <Text style={[styles.simpleText, styles.primaryColor]}>
+                {capitalPrice}
+              </Text>
             </View>
 
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Giá bán</Text>
-              <Text>{sellPrice}</Text>
+              <Text style={[styles.simpleText, styles.primaryColor]}>
+                {sellPrice}
+              </Text>
             </View>
 
             <View style={styles.singleInfoItem}>
               <Text style={styles.simpleInfoHeader}>Tồn kho</Text>
-              <Text>{numberOfProduct}</Text>
+              <Text style={[styles.simpleText, styles.primaryColor]}>
+                {numberOfProduct}
+              </Text>
             </View>
+
+            <ExtendedProductInfoItem
+              title={'Mô tả'}
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
+            <ExtendedProductInfoItem
+              title={'Ghi chú'}
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s"
+            />
           </View>
         </View>
       </ScrollView>
-    </ScrollView>
+    </View>
   );
 };
 
 export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
-  carouselContainer: {
-    marginTop: 20,
+  container: {
+    flex: 1,
   },
+
+  imageSliderContainer: {
+    marginBottom: 10,
+  },
+  scrolledContainer: {},
   productInfoContainer: {
     backgroundColor: 'white',
-    borderTopLeftRadius: 15,
-    borderTopRightRadius: 15,
-    elevation: 5,
+    borderTopLeftRadius: 18,
+    borderTopRightRadius: 18,
+    elevation: 8,
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingBottom: 25,
+  },
+  nameContainer: {
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+  },
+
+  detailsContainer: {},
+
+  mainText: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: TEXT_COLOR,
+  },
+
+  singleInfoItem: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 15,
+    paddingTop: 15,
+  },
+
+  simpleInfoHeader: {
+    fontSize: 16,
+    color: TEXT_COLOR,
+    fontWeight: '500',
+
+    flex: 0.4,
+  },
+
+  simpleText: {
+    fontSize: 18,
+    flex: 0.6,
+  },
+
+  primaryColor: {
+    color: PRIMARY_COLOR,
+    fontWeight: '600',
   },
 });
