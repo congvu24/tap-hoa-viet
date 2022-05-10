b628ae84b29da021053fa9b6fb6c502ffc9276ad
biyasto <biyasto@gmail.com>
2022-05-10 11:56:14 +0700

    change Icon from expo to react-native-vector-icons/Ionicons

--- src/navigation/BottomNavigationBar.js
+++ src/navigation/BottomNavigationBar.js
@@ -14,6 +14,7 @@ import {
   LoginScreen,
   ProductsScreen,
   RegisterScreen,
+  ProfileScreen,
 } from '../screens';
 import HomeScreen from '../screens/Home';
 const Tab = createBottomTabNavigator();
