74d2bc1ebb6bae5893f1ef3f9854553f7b066b66
Nhien Nguyen <53686305+annhienktuit@users.noreply.github.com>
2022-05-10 20:10:30 +0700

    feat:field validation & sua lai phan cau Vu merge mat code

--- src/screens/AddProductScreen.js
+++ src/screens/AddProductScreen.js
@@ -6,18 +6,78 @@ import {
   TouchableOpacity,
   KeyboardAvoidingView,
 } from 'react-native';
-import React, {Component} from 'react';
-import {TextInput} from 'react-native-gesture-handler';
+import React, {Component, useCallback, useState} from 'react';
+import {ScrollView, TextInput} from 'react-native-gesture-handler';
 import HorizontalInputField from '../components/HorizontalInputField';
 import {SafeAreaView} from 'react-native-safe-area-context';
-import {Picker} from '@react-native-picker/picker';
 import PickerWithTitle from '../components/PickerWithTitle';
 import {FAB} from 'react-native-elements';
 import {BACKGROUND_COLOR, WHITE_COLOR} from '../constants/Colors';
+import * as yup from 'yup';
+import {FormProvider, useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import DefaultImage from '../images/ic_upload.png';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
 
-export class AddProductScreen extends Component {
-  render() {
-    return (
+export const AddProductScreen = () => {
+  const schema = yup.object().shape({
+    name: yup.string().required().max(20),
+    quantity: yup.number().required(),
+    price: yup.number().required(),
+    category: yup.string().required(),
+    description: yup.string().required(),
+    importPrice: yup.number().required(),
+    exportPrice: yup.number().required(),
+    qrCode: yup.string(),
+    importDate: yup.string(),
+    exportDate: yup.string(),
+  });
+
+  const formMethod = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmitKey = useCallback(() => {
+    formMethod.handleSubmit(onSubmit)();
+  }, [formMethod, onSubmit]);
+
+  const onSubmit = useCallback(data => {
+    console.log(data);
+  }, []);
+
+  const [filePath, setFilePath] = useState(
+    Image.resolveAssetSource(DefaultImage).uri,
+  );
+
+  const chooseFile = type => {
+    let options = {
+      mediaType: type,
+      maxWidth: 400,
+      maxHeight: 400,
+      quality: 1,
+    };
+    launchImageLibrary(options, response => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        alert('Đã huỷ thao tác');
+        return;
+      } else if (response.errorCode == 'camera_unavailable') {
+        alert('Camera không khả dụng');
+        return;
+      } else if (response.errorCode == 'permission') {
+        alert('Không có quyền truy cập');
+        return;
+      } else if (response.errorCode == 'others') {
+        alert('Fuck cậu Hoàn');
+        return;
+      }
+      console.log(response.assets.map(item => item.uri));
+      setFilePath(String(response.assets.map(item => item.uri)));
+    });
+  };
+
+  return (
+    <FormProvider {...formMethod}>
       <KeyboardAvoidingView
         style={[
           styles.container,
@@ -26,43 +86,73 @@ export class AddProductScreen extends Component {
           },
         ]}
       >
-        <TouchableOpacity style={styles.topContainer}>
-          <Image
-            style={styles.uploadLogo}
-            source={require('../images/ic_upload.png')}
-          />
+        <TouchableOpacity
+          style={styles.uploadButton}
+          onPress={() => chooseFile('photo')}
+        >
+          <Image style={styles.uploadLogo} source={{uri: filePath}} />
         </TouchableOpacity>
 
-        <View style={styles.bottomContainer}>
+        <ScrollView style={styles.bottomContainer}>
           <SafeAreaView>
             <HorizontalInputField
-              title="Mã Hàng"
-              hint="Mã Hàng Tự Động"
-            ></HorizontalInputField>
+              name="name"
+              title="Tên sản phẩm"
+              hint="Nhập tên sản phẩm"
+            />
 
             <HorizontalInputField
-              title="Mã Vạch"
-              hint="Mã Vạch"
-            ></HorizontalInputField>
+              name="description"
+              title="Mô tả"
+              hint="Mô tả sản phẩm"
+            />
 
             <HorizontalInputField
-              title="Tên Hàng"
-              hint="Tên Hàng"
-            ></HorizontalInputField>
+              name="quantity"
+              title="Số lượng"
+              hint="Nhập mã QR Code"
+            />
+
+            <HorizontalInputField
+              name="importPrice"
+              title="Giá nhập"
+              hint="Nhập giá nhập kho"
+            />
+
+            <HorizontalInputField
+              name="exportPrice"
+              title="Giá xuất"
+              hint="Nhập giá xuất kho"
+            />
+
+            <HorizontalInputField
+              name="qrCode"
+              title="Mã QR"
+              hint="Nhập mã QR Code"
+            />
+
+            <HorizontalInputField
+              name="importDate"
+              title="Ngày nhập kho"
+              hint="Ngày nhập kho"
+            />
+
+            <HorizontalInputField
+              name="exportDate"
+              title="Ngày nhập kho"
+              hint="Ngày nhập kho"
+            />
           </SafeAreaView>
 
-          <PickerWithTitle title="Nhóm Hàng" hint="Rau non"></PickerWithTitle>
+          <PickerWithTitle title="Nhóm Hàng" hint="Rau non" />
 
-          <PickerWithTitle
-            title="Thương Hiệu"
-            hint="Liên Xô chấm Mỹ"
-          ></PickerWithTitle>
-        </View>
-        <FAB style={styles.FAB} />
+          <PickerWithTitle title="Thương Hiệu" hint="Liên Xô chấm Mỹ" />
+        </ScrollView>
+        <FAB style={styles.FAB} onPress={onSubmitKey} />
       </KeyboardAvoidingView>
-    );
-  }
-}
+    </FormProvider>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -71,7 +161,6 @@ const styles = StyleSheet.create({
   },
   topContainer: {
     backgroundColor: BACKGROUND_COLOR,
-    flex: 1,
   },
   bottomContainer: {
     flex: 4,
@@ -99,5 +188,6 @@ const styles = StyleSheet.create({
     bottom: 10,
     right: 10,
     icon: 'add',
+    marginBottom: 100,
   },
 });
