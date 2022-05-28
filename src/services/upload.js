d4de89df9b0b8ffc608e599250fc75501ea147ac
Vu Duong <19521110@gm.uit.edu.vn>
2022-05-28 10:34:07 +0700

    upload image

--- src/services/upload.js
+++ src/services/upload.js
@@ -1,13 +1,8 @@
-import {utils} from '@react-native-firebase/app';
 import storage from '@react-native-firebase/storage';
 
 export const uploadFile = async file => {
   try {
     const ref = storage().ref(file.fileName);
-
-    const pathToFile = `${utils.FilePath.PICTURES_DIRECTORY}/black-t-shirt-sm.png`;
-    // uploads file
-    console.log(file)
     await ref.putFile(file.uri);
 
     return ref.getDownloadURL();
